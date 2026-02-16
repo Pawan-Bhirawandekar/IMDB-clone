a5ee435d0008e1929f10147fca12c6f269ce76f6
Pawan Bhirawandekar <pawanbhirawandekar@gmail.com>
2026-02-16 20:54:24 +0530

    Add Watchlist component and MoviCard for displaying trending movies

---
+++ IMDB_Clone/src/component/MoviCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MoviCard = () => {
+  return (
+    <div className='h-[40vh] w-50 bg-cover bg-center rounded-xl hover:scale-110 duration-300 hover:cursor-pointer ' style={{backgroundImage:`url(https://media-cache.cinematerial.com/p/500x/dddxsrkw/chhaava-indian-movie-poster.jpg?v=1724329223)`}}></div>
+  )
+}
+
+export default MoviCard
--- IMDB_Clone/src/component/Movies.jsx
+++ IMDB_Clone/src/component/Movies.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import MoviCard from './MoviCard'
 
 const  Watchlist= () => {
   return (
     <div>
-        <h1>Movies Component</h1>
+        <div className='text-2xl font-bold text-center p-2'>
+          Trending Movies
+        </div>
+
+        <div className='p-4 flex flex-wrap gap-4 justify-center'>
+          <MoviCard/>
+          <MoviCard/>
+          <MoviCard/>
+          <MoviCard/>
+          <MoviCard/>
+          <MoviCard/>
+          <MoviCard/>
+          <MoviCard/>
+          <MoviCard/>
+          <MoviCard/>
+        </div>
     </div>
   )
 }
